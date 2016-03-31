bd8b689c215e9ddbd0de381472e1047c3d62a688
MTRNord <mtrnord1@gmail.com>
2016-03-31 21:22:26 +0200

    [BUG] Fix encoding

--- src/main/java/de/nordgedanken/spigotnordgedanken/Listener/Tablist.java
+++ src/main/java/de/nordgedanken/spigotnordgedanken/Listener/Tablist.java
@@ -14,19 +14,19 @@ import org.inventivetalent.tabapi.TabItem;
 public class Tablist implements Listener{
 	public void setHeaderAndFooter(Player player) {
         TabAPI.setHeader(player,
-                "§9Welcome to the server, ",
-                "§a" + player.getName() + "!");
+                "Â§9Welcome to the server, ",
+                "Â§a" + player.getName() + "!");
 
         TabAPI.setFooter(player,
-                "§2Online players",
-                "§7" + Bukkit.getOnlinePlayers().size() + "§8/§6100");
+                "Â§2Online players",
+                "Â§7" + Bukkit.getOnlinePlayers().size() + "Â§8/Â§6100");
     }
 	public void setItems(Player player) {
         TabAPI.clearAllItems(player);//Remove all items
 
         TabAPI.setItems(player, Arrays.asList(//Add our custom items
-                new TabItem("§0§c§lAdmins"),
-                new TabItem("§1§fYou!")));
+                new TabItem("Â§0Â§cÂ§lAdmins"),
+                new TabItem("Â§1Â§fYou!")));
     }
 	 @EventHandler (priority = EventPriority.LOWEST)
      public void onPlayerJoin(PlayerJoinEvent event)
